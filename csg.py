45f0fe6a7b16ea88f4cda8c18ea41d3f1c82ca25
Adam <adm.balig@gmail.com>
2025-09-12 22:26:42 +0200

    basics of csg and the raymarcher

---
+++ csg.py
@@ -0,0 +1,123 @@
+import numpy as np
+
+class Primitive: 
+    #the class
+    def __init__(self):
+        #We create variables that store the transformations performed on the object
+        #this allows us to consider the signed distance function(SDF) 
+        self.translation = np.zeros(3)
+        self.rotation = np.eye(3)
+
+    
+    def reverse_transform(self,p:np.array):
+        # this method applies the inverse transformation to a given p
+        p = p - self.translation
+        p = p @ self.rotation
+        return p
+
+class Box(Primitive):
+    def __init__(self,length=1,width=1,height=1):
+        super().__init__()
+        self.side_lengths = np.array([length,width,height])
+
+    def sdf(self,p:np.array):
+        p = self.reverse_transform(p)
+        #since the box has axial symmetry we can consider the p where all coordinates are positive
+        p = np.abs(p)
+        q = p - (self.side_lengths / 2)
+        distance = np.linalg.norm(np.maximum(q,0)) + min(0,max(q[0],q[1],q[2]))
+        return distance
+        
+
+class Sphere(Primitive):
+    def __init__(self,radius=1):
+        super().__init__()
+        self.r = radius
+    
+    def sdf(self,p:np.array):
+        p = self.reverse_transform(p)
+        distance = np.linalg.norm(p) - self.r
+        return distance
+
+
+
+class Cylinder(Primitive):
+    def __init__(self,radius=1,height=1):
+        super().__init__()
+        self.r = radius
+        self.h = height
+
+    
+    def sdf(self,p:np.array):
+        p = self.reverse_transform(p)
+        p = np.abs(p)
+
+        d = np.array([np.linalg.norm(p[:2]) - self.r, p[2] - self.h / 2])
+        distance = np.minimum(np.maximum(d[0], d[1]), 0) + np.linalg.norm(np.maximum(d, 0))
+        return distance
+
+#We will represent the CSG object as a binary tree, each node will either be a primitive or an operator
+#we can observe that the leaves of our binary tree will always be primitives and the other nodes will be operators
+class CSG_object_node:
+    def __init__(self,primitive=None,operator=None,left=None,right = None):
+        self.primitive = primitive 
+        self.operator = operator
+        self.left = left
+        self.right = right
+
+    def is_leaf(self):
+        #this funtion determines whether the node is a leaf becuase only the lease
+        return self.primitive != None
+   
+    def sdf(self,p:np.array) -> np.float32:
+        if self.is_leaf():
+            return self.primitive.sdf(p)
+        else:
+            left_dist = self.left.sdf(p)
+            right_dist = self.right.sdf(p)
+            if self.operator == 'union': 
+                # this operation is not perfect while it determines the exterior distance correctly
+
+                #TODO think about how to fix this
+                return min(left_dist, right_dist) 
+            elif self.operator == 'intersection':
+                return max(left_dist, right_dist)
+            elif self.operator == 'difference':
+                return max(left_dist, -right_dist)
+            else:
+                raise ValueError("Unknown operator")
+    def translate(self,v:np.array ):
+        if self.is_leaf():
+            self.translation = self.translation + v
+        else:
+            self.left.translate(v)
+            self.right.translate(v)
+
+    def rotate(self,inverse_rot_matrix): # the input should be the inverse rotation matrix
+        if self.is_leaf():
+            self.rotation = self.rotation @ inverse_rot_matrix
+        else:
+            self.left.rotate(inverse_rot_matrix)
+            self.right.rotate(inverse_rot_matrix)
+
+# We define funtions whose input is are two CSG nodes and output is a new CSG node with the appropriate operator
+
+def CSG_union(obj1:CSG_object_node,obj2:CSG_object_node):
+    return CSG_object_node(operator="union",left = obj1,right=obj2)
+
+def CSG_intersection(obj1:CSG_object_node,obj2:CSG_object_node):
+    return CSG_object_node(operator="intersection",left = obj1,right=obj2)
+
+def CSG_difference(obj1:CSG_object_node,obj2:CSG_object_node):
+    return CSG_object_node(operator="difference",left = obj1,right=obj2)
+
+
+
+
+
+
+ 
+
+
+
+
--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello wor")
+import csg
+import ray_marching
+
+import tkinter as tk
+
+WIDTH = 400
+HEIGHT = 600 
+
+window = tk.Tk()
+window.title("Adams CSG")
+
+canvas = tk.Canvas(window,width=WIDTH,height=HEIGHT,bg="white")
+canvas.pack()
+
